d4c51c55abd51ebff4c39510574559c92a0a676d
lalgwebdev <webtech@lalg.org.uk>
2019-11-29 13:21:33 +0200

    Add PP for BACS (Offline) Payment Method

--- cashpp.mgd.php
+++ cashpp.mgd.php
@@ -20,12 +20,12 @@ return [
     ],
   ],
   1 => [
-    'name' => 'check',
+    'name' => 'cheque',
     'entity' => 'payment_processor_type',
     'params' => [
       'version' => 3,
-      'title' => 'Check',
-      'name' => 'check',
+      'title' => 'Cheque',
+      'name' => 'cheque',
       'description' => 'Payment processor for recording check contributions',
       'user_name_label' => 'Username (not used)',
       'password_label' => 'Password (not used)',
@@ -33,7 +33,25 @@ return [
       'url_site_test_default' => 'https://example.org',
       'class_name' => 'Payment_Manual',
       'billing_mode' => 1, // 1=form
-      'payment_type' => 4, // 4=check
+      'payment_type' => 4, // 4=cheque
+      'is_recur' => 0,
+    ],
+  ],
+  2 => [
+    'name' => 'bacs',
+    'entity' => 'payment_processor_type',
+    'params' => [
+      'version' => 3,
+      'title' => 'BACS Offline',
+      'name' => 'bacs',
+      'description' => 'Payment processor for recording Offline BACS contributions',
+      'user_name_label' => 'Username (not used)',
+      'password_label' => 'Password (not used)',
+      'url_site_default' => 'https://example.org',
+      'url_site_test_default' => 'https://example.org',
+      'class_name' => 'Payment_Manual',
+      'billing_mode' => 1, // 1=form
+      'payment_type' => 5, // 5=BACS Offline (was EFT)
       'is_recur' => 0,
     ],
   ],
